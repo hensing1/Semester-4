c4dd5febdaac322b0fb90cc51b15853f79799a6d
Lukas Albani <lukasalbani@Lukas-MacBook-Pro.local>
2019-04-10 12:21:22 +0200

    Uebung Hashtables

--- src/AbstraktUndCo/Main.java
+++ src/AbstraktUndCo/Main.java
@@ -15,7 +15,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         
-        UebungsaufgabenCollections();
+        UebungsHashTables();
         
     }
     
@@ -29,5 +29,27 @@ public class Main {
         for (int i= 0; i < vWieVendetta.size(); i++) {
             System.out.println(vWieVendetta.get(i).toString());
         }
+    }
+
+
+    public static void UebungsHashTables() {
+        String[] marken = {"bmw", "mercedes", "vw", "bentley", "porsche"};
+        
+        int[] baujahr = {1997, 2008, 2019, 2018, 1999};
+        Hashtable ht = new Hashtable();
+        
+        
+        for (int i = 0; i < marken.length; i++) {
+           ht.put(marken[i], new Auto(baujahr[i],false, 4, 333));
+            // autoboxing -- "4711" wird zu einem Objekt der Wrapper-Klasse Integer
+        }
+        
+        Enumeration e = ht.elements();
+        
+        while (e.hasMoreElements()) {
+            Auto mycar = (Auto) e.nextElement();
+            System.out.println("Baujahr: " + mycar.getBaujahr());
+        }
+        
     }
 }
