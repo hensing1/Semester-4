7169274eedf0a6c5f7a35f522d7d22f530c5d7f7
Jens Lehmann <jenshenning.lehmann@rfh-campus.de>
2019-06-12 01:37:50 +0200

    interfaces

---
+++ src/AbstraktUndCo/NochMehrTest.java
@@ -0,0 +1,16 @@
+package AbstraktUndCo;
+
+public class NochMehrTest implements Test {
+
+    /*@Override
+    public int gibMichMeinInt() {
+        //digga nö
+        // hi = 6; GEHT NICHT WEIL FINAL AHA
+        return hi + 1;
+    }*/
+
+    @Override
+    public void troll() {
+        int ichbinnevariable = Test.hi; //BOAH DAS GEHT WUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU
+    }
+}
