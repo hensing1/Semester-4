96b5707d72bce95034e7addbc8bde48910681943
Henning Lehmann <34438247+henry-dv@users.noreply.github.com>
2019-06-26 11:28:51 +0200

    sum tin wong

---
+++ src/SocketInMyPocket/ClientEins.java
@@ -0,0 +1,37 @@
+package SocketInMyPocket;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jens Henning Lehmann; 213172003
+ */
+public class ClientEins {
+    public static void main(String[] args) {
+        System.out.println("Willkommen zum coolen Client");
+        ClientEins c = new ClientEins();
+        c.sendToServer("Toller Teststring 1");
+        c.sendToServer("Toller Teststring 2");
+        c.sendToServer("Toller Teststring 3");
+        c.sendToServer("exit");
+    }
+    
+    private void sendToServer(String msg) {
+        try {
+            Socket cs = new Socket("localhost", 4000);
+            PrintWriter out = new PrintWriter(cs.getOutputStream());
+            out.println(msg);
+            
+            System.out.println(String.format("Gesendet: %s", msg));
+            
+            out.close();
+            cs.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientEins.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
